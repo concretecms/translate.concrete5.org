69ba47fe22604ff4eaccb8bf0f990dbf5f24f943
Michele Locati <michele@locati.it>
2017-04-05 17:58:31 +0200

    Show translation-related stuff in profile pages

--- public/application/single_pages/members/profile.php
+++ public/application/single_pages/members/profile.php
@@ -1,12 +1,28 @@
 <?php
+use CommunityTranslation\Service\Access;
+use CommunityTranslation\Repository\Locale as LocaleRepository;
 use Concrete\Core\Authentication\AuthenticationType;
+use Concrete\Core\Block\Block;
+use Concrete\Core\Database\Connection\Connection;
+use Concrete\Core\Support\Facade\Application;
 
 defined('C5_EXECUTE') or die('Access Denied.');
 
-/* @var Concrete\Core\User\UserInfo $profile */
+/* @var Concrete\Core\Page\Page $c */
+/* @var Concrete\Core\Page\View\PageView $view */
+/* @var Concrete\Core\Html\Service\Html $html */
+/* @var Concrete\Controller\SinglePage\Members\Profile $controller */
 
-$dh = Core::make('helper/date');
+/* @var Concrete\Core\User\UserInfo $profile */
+/* @var array $badges */
+
+$app = Application::getFacadeApplication();
+
+$dh = $app->make('date');
 /* @var Concrete\Core\Localization\Service\Date $dh */
+
+$url = $app->make('url/manager');
+/* @var Concrete\Core\Url\Resolver\Manager\ResolverManager $url */
 
 $c5ProfileURL = null;
 try {
@@ -28,8 +44,8 @@ try {
         if ($canEdit) {
             ?>
             <div class="btn-group">
-                <a href="<?= $view->url('/account/edit_profile') ?>" class="btn btn-sm btn-default"><i class="fa fa-cog"></i> <?= t('Edit') ?></a>
-                <a href="<?= $view->url('/') ?>" class="btn btn-sm btn-default"><i class="fa fa-home"></i> <?= t('Home') ?></a>
+                <a href="<?= $url->resolve(['/account/edit_profile']) ?>" class="btn btn-sm btn-default"><i class="fa fa-cog"></i> <?= t('Edit') ?></a>
+                <a href="<?= $url->resolve(['/']) ?>" class="btn btn-sm btn-default"><i class="fa fa-home"></i> <?= t('Home') ?></a>
             </div>
             <?php
         } else {
@@ -39,7 +55,7 @@ try {
                     ?><a href="<?= h($c5ProfileURL) ?>" class="btn btn-sm btn-default" target="_blank"><i class="fa-user fa"></i> <?= t('View concrete5 profile') ?></a><?php
                 }
                 if ($profile->getAttribute('profile_private_messages_enabled')) {
-                    ?><a href="<?= $view->url('/account/messages', 'write', $profile->getUserID()) ?>" class="btn btn-sm btn-default"><i class="fa-envelope fa"></i> <?= t('Send private message') ?></a><?php
+                    ?><a href="<?= $url->resolve(['/account/messages', 'write', $profile->getUserID()]) ?>" class="btn btn-sm btn-default"><i class="fa-envelope fa"></i> <?= t('Send private message') ?></a><?php
                 }
                 ?>
             </div>
@@ -114,6 +130,98 @@ try {
             </ul>
             <?php
         }
+        
+        if (class_exists(Access::class) && $profile->getUserID() != USER_SUPER_ID) {
+            $access = $app->make(Access::class);
+            /* @var Access $access */
+            $localeRepository = $app->make(LocaleRepository::class);
+            /* @var LocaleRepository $localeRepository */
+            $localeAccessList = [];
+            $teamsPage = false;
+            foreach ($localeRepository->getApprovedLocales() as $locale) {
+                $la = $access->getLocaleAccess($locale, $profile->getEntityObject());
+                if ($la >= Access::GLOBAL_ADMIN) {
+                    $localeAccessList = Access::GLOBAL_ADMIN;
+                    break;
+                } else {
+                    switch ($la) {
+                        case Access::ADMIN:
+                            $text = tc('User is...', 'a team coordinator for %s');
+                            break;
+                        case Access::TRANSLATE:
+                            $text = tc('User is...', 'a translator for %s');
+                            break;
+                        case Access::ASPRIRING:
+                            $text = tc('User is...', 'an aspiring translator for %s');
+                            break;
+                        default:
+                            $text = null;
+                            break;
+                    }
+                    if ($text !== null) {
+                        if ($teamsPage === false) {
+                            $teamsPage = null;
+                            $block = Block::getByName('CommunityTranslation Team List');
+                            if ($block && $block->getBlockID()) {
+                                $p = $block->getOriginalCollection();
+                                if ($p && !$p->isError()) {
+                                    $teamsPage = $p;
+                                }
+                            }
+                        }
+                        if ($teamsPage === null) {
+                            $text = sprintf($text, h($locale->getDisplayName()));
+                        } else {
+                            $text = sprintf($text, sprintf('<a href="%s">%s</a>', $url->resolve([$teamsPage, 'details', $locale->getID()]), h($locale->getDisplayName())));
+                        }
+                        $localeAccessList[] = $text;
+                    }
+                }
+            }
+            if ($localeAccessList !== []) {
+                ?>
+                <h4><?= t('Translator Details') ?></h4>
+                <?php
+                if ($localeAccessList === Access::GLOBAL_ADMIN) {
+                    ?><p><?= t('%s is a site maintainer: your last hope to solve localization-related issues.', h($profile->getUserName())) ?></p><?php
+                } else {
+                    ?><p><?= tc('User is...', '%1$s is %2$s.', h($profile->getUserName()), Punic\Misc::join($localeAccessList)) ?></p><?php
+                }
+                $db = $app->make(Connection::class);
+                $totalTranslations = 0;
+                $currentTranslations = 0;
+                /* @var Connection $db */
+                $rs = $db->executeQuery('select count(*) as n, current from CommunityTranslationTranslations where createdBy = ? group by current', [$profile->getUserID()]);
+                while (($row = $rs->fetch()) !== false) {
+                    if ($row['current']) {
+                        $currentTranslations = (int) $row['n'];
+                        $totalTranslations += $currentTranslations;
+                    } else {
+                        $totalTranslations += (int) $row['n'];
+                    }
+                }
+                $rs->closeCursor();
+                if ($totalTranslations === 0) {
+                    ?><p><?= t('%s has not yet contributed any translation.', h($profile->getUserName())) ?></p><?php
+                } else {
+                    ?><p><?=
+                        t2(
+                            '%2$s has contributed with %1$d translation',
+                            '%2$s has contributed with %1$d translations',
+                            $totalTranslations,
+                            h($profile->getUserName())
+                        )
+                        .
+                        t2(
+                            '(%d of which is a currently approved translation)',
+                            '(%d of which are currently approved translations)',
+                            $currentTranslations
+                        )
+                    ?>.</p><?php
+                }
+            }
+        }
+        
 
         $a = new Area('Main');
         //$a->setAttribute('profile', $profile);
