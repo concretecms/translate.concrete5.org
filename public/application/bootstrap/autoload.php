0bc8bbb9c64a35273a439f8040dc7e48302e3571
Korvin Szanto <Korvinszanto@gmail.com>
2017-05-02 09:59:26 -0700

    Add dotenv support so that we can include environment variables easily (#9)
    * Include a token with download requests
    
    * Add a .deploy file as well to track the current commit

--- public/application/bootstrap/autoload.php
+++ public/application/bootstrap/autoload.php
@@ -7,3 +7,15 @@ require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 # Add the vendor directory to the include path
 ini_set('include_path', dirname(__DIR__, 3) . "/vendor" . PATH_SEPARATOR . get_include_path());
 
+# Include the environment variables
+try {
+    // Include the env
+    $dotenv = new Dotenv\Dotenv(dirname(__DIR__, 3));
+    $dotenv->load();
+
+    // Include the deploy number
+    $dotenv = new Dotenv\Dotenv(dirname(__DIR__, 3), '.deploy');
+    $dotenv->load();
+} catch (\Dotenv\Exception\InvalidPathException $e) {
+    // .env file doesn't exist.
+}
