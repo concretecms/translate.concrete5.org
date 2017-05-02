eee2cf499b651d50861bafe9ab873faa8a54e7d3
Korvin Szanto <korvinszanto@gmail.com>
2017-05-02 08:46:19 -0700

    Include a token with download requests

--- public/application/bootstrap/autoload.php
+++ public/application/bootstrap/autoload.php
@@ -7,3 +7,10 @@ require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 # Add the vendor directory to the include path
 ini_set('include_path', dirname(__DIR__, 3) . "/vendor" . PATH_SEPARATOR . get_include_path());
 
+# Include the environment variables
+try {
+    $dotenv = new Dotenv\Dotenv(dirname(__DIR__, 3));
+    $dotenv->load();
+} catch (\Dotenv\Exception\InvalidPathException $e) {
+    // .env file doesn't exist.
+}
