b7564e663887ff8e62d8f9c8beb0df751cef71dd
Michele Locati <michele@locati.it>
2017-05-03 21:16:18 +0200

    Fix bug introduced in https://github.com/concrete5/translate.concrete5.org/pull/11

--- src/Http/TimingMiddleware.php
+++ src/Http/TimingMiddleware.php
@@ -27,7 +27,7 @@ class TimingMiddleware implements MiddlewareInterface
             $response->setContent($body);
         }
 
-        return $response->setContent($body);
+        return $response;
     }
 
     /**
